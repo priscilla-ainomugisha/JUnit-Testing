bcf66f6d01b4ca68d4df9424dbbdeda1497e6345
MartinKalema <kalemaaaaaaaa@gmail.com>
2024-03-24 01:47:59 +0300

    Added two tests to CalendarTest.jave

--- meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/CalendarTest.java
+++ meetingplanner/src/test/java/edu/sc/bse3211/meetingplanner/CalendarTest.java
@@ -28,19 +28,36 @@ public class CalendarTest {
 		}
 	}
 
+	// What happens when a meeting is scheduled on a day that is already booked? 
 	@Test
-	public void testIsBusy(){
+	public void testAddMeetingSameDay(){
 		try{
 			Meeting firstMeeting = new Meeting(9, 10, "First Meeting");
 			calendar.addMeeting(firstMeeting);
-			assertTrue("First meeting added to the calendar", calendar.isBusy(9, 10, 0, 23));
+			assertTrue("Meeting Scheduled", calendar.isBusy(9, 10, 0, 23));
+			calendar.addMeeting(new Meeting(9, 10, "Second Meeting"));
+		} catch(TimeConflictException e){
+			fail("Method addMeeting() has thrown the exception: " + e.getMessage());
+		}
+	}
 
-			// Adding another meeting on the same day
-			Meeting secondMeeting = new Meeting(9, 10, "Second Meeting");
-			calendar.addMeeting(secondMeeting);
-
+	// Does the clearSchedule() method work?
+	@Test
+	public void testClearSchedule(){
+		try{
+			Meeting testMeeting = new Meeting(2,10, "Test Meeting");
+			calendar.addMeeting(testMeeting);
+			calendar.clearSchedule(2, 10);
+			assertTrue("Date not busy",!(calendar.isBusy(2, 10, 0, 23)));
+			
 		} catch(TimeConflictException e){
 			fail("Should not throw exception: " + e.getMessage());
 		}
+	}
+
+	// Does clearSchedule() output the appropriate error message when no meeting was scheduled?
+	@Test
+	public void testClearScheduleOnDayWithNoMeeting(){
+		calendar.clearSchedule(1,4);
 	}
 }
